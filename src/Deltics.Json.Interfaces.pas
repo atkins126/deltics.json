e2ceee0d97caca02916af39a35c6b161e0a9a9b7
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-02-08 20:41:17 +1300

    feat: Json Interfaces

--- src/Deltics.Json.Interfaces.pas
+++ src/Deltics.Json.Interfaces.pas
@@ -7,10 +7,12 @@
 interface
 
   uses
+    TypInfo,
     Deltics.Strings;
 
 
   type
+    TNumberType = (ntDouble, ntInteger);
     TValueType = (jsObject, jsArray, jsString, jsNumber, jsBoolean, jsNull);
 
 
@@ -23,20 +25,49 @@ interface
 
     IJsonValue = interface
     ['{836B3B1C-9590-4568-B65A-92ED284F1116}']
+      function get_AsBoolean: Boolean;
+      function get_AsCardinal: Cardinal;
+      function get_AsDate: TDate;
+      function get_AsTime: TTime;
+      function get_AsDateTime: TDateTime;
+      function get_AsDouble: Double;
+      function get_AsEnum(const aTypeInfo: PTypeInfo): Integer;
+      function get_AsExtended: Extended;
+      function get_AsGuid: TGuid;
+      function get_AsInt64: Int64;
+      function get_AsInteger: Integer;
       function get_AsString: UnicodeString;
-      function get_ValueType: TValueType;
-      procedure set_AsString(const aValue: UnicodeString);
-
-      property AsString: UnicodeString read get_AsString write set_AsString;
-      property ValueType: TValueType read get_ValueType;
-    end;
-
-    IJsonString = interface(IJsonValue)
-    ['{7A4790EF-B430-467B-86EA-9B85A7A5557D}']
+      function get_IsNull: Boolean;
       function get_Value: Utf8String;
+      function get_ValueType: TValueType;
+      procedure set_AsBoolean(const aValue: Boolean);
+      procedure set_AsCardinal(const aValue: Cardinal);
+      procedure set_AsDate(const aValue: TDate);
+      procedure set_AsTime(const aValue: TTime);
+      procedure set_AsDateTime(const aValue: TDateTime);
+      procedure set_AsDouble(const aValue: Double);
+      procedure set_AsExtended(const aValue: Extended);
+      procedure set_AsGuid(const aValue: TGuid);
+      procedure set_AsInt64(const aValue: Int64);
+      procedure set_AsInteger(const aValue: Integer);
+      procedure set_AsString(const aValue: UnicodeString);
       procedure set_Value(const aValue: Utf8String);
 
+      property AsBoolean: Boolean read get_AsBoolean write set_AsBoolean;
+      property AsCardinal: Cardinal read get_AsCardinal write set_AsCardinal;
+      property AsDate: TDate read get_AsDate write set_AsDate;
+      property AsTime: TTime read get_AsTime write set_AsTime;
+      property AsDateTime: TDateTime read get_AsDateTime write set_AsDateTime;
+      property AsDouble: Double read get_AsDouble write set_AsDouble;
+      property AsEnum[const aTypeInfo: PTypeInfo]: Integer read get_AsEnum;
+      property AsExtended: Extended read get_AsExtended write set_AsExtended;
+      property AsGuid: TGuid read get_AsGuid write set_AsGuid;
+      property AsInt64: Int64 read get_AsInt64 write set_AsInt64;
+      property AsInteger: Integer read get_AsInteger write set_AsInteger;
+      property AsString: UnicodeString read get_AsString write set_AsString;
+      property IsNull: Boolean read get_IsNull;
       property Value: Utf8String read get_Value write set_Value;
+      property ValueType: TValueType read get_ValueType;
     end;
 
 
@@ -44,6 +75,8 @@ interface
     ['{5F178757-189B-4930-972B-2DB76A34BC3B}']
       function get_Count: Integer;
       function get_IsEmpty: Boolean;
+
+      procedure Clear;
 
       property Count: Integer read get_Count;
       property IsEmpty: Boolean read get_IsEmpty;
@@ -55,55 +88,47 @@ interface
       function get_Item(const aIndex: Integer): IJsonMember;
       function get_Value(const aName: UnicodeString): IJsonValue;
 
-      procedure Add(const aName: UnicodeString; const aValue: IJsonValue);
+      function Add(const aName: UnicodeString; const aValue: IJsonValue): IJsonMember;
+      function Contains(const aName: UnicodeString): Boolean; overload;
+      function Contains(const aName: UnicodeString; var aMember: IJsonMember): Boolean; overload;
 
       property Items[const aIndex: Integer]: IJsonMember read get_Item;
       property Value[const aName: UnicodeString]: IJsonValue read get_Value; default;
-
-(*
-      function DoGetAsString: UnicodeString; override;
-      procedure DoSetAsString(const aValue: UnicodeString); override;
-    public
-      class function TryCreate(const aString: UnicodeString): TJsonObject;
-      constructor Create; overload; override;
-      constructor Create(const aString: UnicodeString); reintroduce; overload;
-      constructor CreateFromFile(const aFilename: UnicodeString; const aEncoding: TEncoding);
-      destructor Destroy; override;
-      function AddAfter(const aPredecessor: UnicodeString; const aName: UnicodeString; const aValue: UnicodeString): TJsonString; overload;
-      procedure Clear; override;
-      function Clone: TJsonObject; reintroduce;
-      procedure Combine(const aObject: TJsonObject);
-      function Contains(const aValueName: UnicodeString): Boolean; overload;
-      function Contains(const aValueName: UnicodeString; var aValue: UnicodeString): Boolean; overload;
-      function Contains(const aValueName: UnicodeString; var aValue: TJsonArray): Boolean; overload;
-      function Contains(const aValueName: UnicodeString; var aValue: TWIDEStringArray): Boolean; overload;
-      procedure Delete(const aValueName: UnicodeString); overload;
-      function FindValue(const aName: UnicodeString): TJsonValue;
-      procedure CopyFrom(const aSource: TJsonValue); override;
-      procedure LoadFromStream(const aStream: TStream; const aEncoding: TEncoding = NIL);
-      function OptBoolean(const aName: UnicodeString; const aDefault: Boolean = FALSE): Boolean;
-      function OptDateTime(const aName: UnicodeString; const aDefault: TDateTime = 0): TDateTime;
-      function OptEnum(const aName: UnicodeString; const aTypeInfo: PTypeInfo; const aDefault: Integer = 0): Integer;
-      function OptInteger(const aName: UnicodeString; const aDefault: Integer = 0): Integer;
-      function OptString(const aName: UnicodeString; const aDefault: UnicodeString = ''): UnicodeString;
-      procedure Wipe; override;
-    {$ifdef DELPHI2009__}
-      function OptDate(const aName: UnicodeString; const aDefault: TDate = 0): TDate;
-      function OptTime(const aName: UnicodeString; const aDefault: TTime = 0): TTime;
-    {$endif}
-*)
     end;
 
 
     IJsonMember = interface
     ['{61F3F493-B3CB-4A60-A7D2-4E76EA197FAD}']
-      function get_Name: Utf8String;
+      function get_Name: UnicodeString;
       function get_Value: IJsonValue;
-      procedure set_Name(const aValue: Utf8String);
+      procedure set_Name(const aValue: UnicodeString);
       procedure set_Value(const aValue: IJsonValue);
 
-      property Name: Utf8String read get_Name write set_Name;
+      function ValueOrDefault(aDefault: IJsonValue): IJsonValue;
+
+      property Name: UnicodeString read get_Name write set_Name;
       property Value: IJsonValue read geT_Value write set_Value;
+    end;
+
+
+    IJsonArray = interface(IJsonCollection)
+    ['{9F5563F5-D0BB-4DDA-9300-FFC7FC941C3B}']
+      function get_Item(const aIndex: Integer): IJsonValue;
+
+      function Add(const aValue: IJsonValue): Integer;
+      procedure Delete(const aIndex: Integer); overload;
+      procedure Delete(const aValue: IJsonValue); overload;
+
+      property Items[const aIndex: Integer]: IJsonValue read get_Item; default;
+    end;
+
+
+
+    IJsonReader = interface
+    ['{92F4B915-D0AE-436D-B6FA-952ABABD4B46}']
+      function ReadArray: IJsonArray;
+      function ReadObject: IJsonObject;
+      function ReadValue: IJsonValue;
     end;
 
 

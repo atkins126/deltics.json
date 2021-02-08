d9a8783e970484f4a5f8b9d0016eb83b3008502d
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-02-08 20:57:46 +1300

    chore: Tidy up uses list

--- tests/Test.dpr
+++ tests/Test.dpr
@@ -9,21 +9,21 @@ uses
   FastMM4,
   Deltics.Smoketest,
   Deltics.Json in '..\src\Deltics.Json.pas',
+  Deltics.Json.Array_ in '..\src\Deltics.Json.Array_.pas',
   Deltics.Json.Collection in '..\src\Deltics.Json.Collection.pas',
   Deltics.Json.Exceptions in '..\src\Deltics.Json.Exceptions.pas',
+  Deltics.Json.Factories in '..\src\Deltics.Json.Factories.pas',
+  Deltics.Json.Formatter in '..\src\Deltics.Json.Formatter.pas',
   Deltics.Json.Interfaces in '..\src\Deltics.Json.Interfaces.pas',
   Deltics.Json.Member in '..\src\Deltics.Json.Member.pas',
   Deltics.Json.MemberValue in '..\src\Deltics.Json.MemberValue.pas',
   Deltics.Json.Object_ in '..\src\Deltics.Json.Object_.pas',
+  Deltics.Json.Reader in '..\src\Deltics.Json.Reader.pas',
   Deltics.Json.Value in '..\src\Deltics.Json.Value.pas',
   Deltics.Json.Utils in '..\src\Deltics.Json.Utils.pas',
-  Deltics.Json.Factories in '..\src\Deltics.Json.Factories.pas',
   Test.BuildingJson in 'Test.BuildingJson.pas',
   Test.NumberConversions in 'Test.NumberConversions.pas',
-  Deltics.Json.Reader in '..\src\Deltics.Json.Reader.pas',
-  Deltics.Json.Array_ in '..\src\Deltics.Json.Array_.pas',
-  Test.JsonReader in 'Test.JsonReader.pas',
-  Deltics.Json.Formatter in '..\src\Deltics.Json.Formatter.pas';
+  Test.JsonReader in 'Test.JsonReader.pas';
 
 begin
   TestRun.Test(BuildingJson);

9c653db1f7749a28a930004150aaea81ca335062
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-02-14 09:39:59 +1300

    test: Tests for string encoding/decoding

---
+++ tests/Test.JsonStringDecoding.pas
@@ -0,0 +1,181 @@
+unit Test.JsonStringDecoding;
+
+interface
+
+  uses
+    Deltics.Smoketest;
+
+
+  type
+    JsonStringDecoding = class(TTest)
+      procedure RemovesDoubleQuotes;
+      procedure AsciiCharacterStringsDecodeWithoutEscaping;
+      procedure DoubleQuoteDecodesWithEscaping;
+      procedure ReverseSolidusDecodesWithEscaping;
+      procedure SolidusDecodesWithEscaping;
+      procedure BackspaceDecodesWithEscaping;
+      procedure FormFeedDecodesWithEscaping;
+      procedure LineFeedDecodesWithEscaping;
+      procedure CarriageReturnDecodesWithEscaping;
+      procedure TabDecodesWithEscaping;
+      procedure OtherControlCharactersDecodeWithEscapedUnicode;
+      procedure NonAsciiCharactersDecodeWithEscapedUnicode;
+      procedure SupplementaryCharacterStringsDecodeWithEscapedUnicode;
+    end;
+
+
+
+implementation
+
+  uses
+    Deltics.Strings,
+    Deltics.Json;
+
+
+
+{ JsonStringDecoding }
+
+  procedure JsonStringDecoding.AsciiCharacterStringsDecodeWithoutEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('abc def');
+
+    Test('s').Assert(s).Equals('abc def');
+  end;
+
+
+  procedure JsonStringDecoding.SolidusDecodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\/');
+
+    Test('s').Assert(s).Equals('/');
+  end;
+
+
+  procedure JsonStringDecoding.BackspaceDecodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\b');
+
+    Test('s').Assert(s).Equals(#8);
+  end;
+
+
+  procedure JsonStringDecoding.CarriageReturnDecodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\r');
+
+    Test('s').Assert(s).Equals(#13);
+  end;
+
+
+  procedure JsonStringDecoding.DoubleQuoteDecodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\"');
+
+    Test('s').Assert(s).Equals('"');
+  end;
+
+
+  procedure JsonStringDecoding.FormFeedDecodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\f');
+
+    Test('s').Assert(s).Equals(#12);
+  end;
+
+
+  procedure JsonStringDecoding.LineFeedDecodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\n');
+
+    Test('s').Assert(s).Equals(#10);
+  end;
+
+
+  procedure JsonStringDecoding.NonAsciiCharactersDecodeWithEscapedUnicode;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\u00A9');
+
+    Test('s').Assert(s).Equals('©');
+  end;
+
+
+  procedure JsonStringDecoding.OtherControlCharactersDecodeWithEscapedUnicode;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\u000B');
+
+    Test('s').Assert(s).Equals(#11);
+  end;
+
+
+  procedure JsonStringDecoding.RemovesDoubleQuotes;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('"abc def"');
+
+    Test('s').Assert(s).Equals('abc def');
+  end;
+
+
+  procedure JsonStringDecoding.ReverseSolidusDecodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\\');
+
+    Test('s').Assert(s).Equals('\');
+  end;
+
+
+  procedure JsonStringDecoding.SupplementaryCharacterStringsDecodeWithEscapedUnicode;
+  {
+    For this test we use Unicode codepoint Ux1d11e - the treble clef
+
+      UTF-8 Decoding  :	0xF0 0x9D 0x84 0x9E
+      UTF-16 Decoding :	0xD834 0xDD1E
+  }
+  var
+    src: UnicodeString;
+    s: UnicodeString;
+  begin
+    src  := '??';
+    src[1] := WideChar($d834);
+    src[2] := WideChar($dd1e);
+
+    s := Json.DecodeString('\uD834\uDD1E');
+
+    Test('s[1]').Assert(Word(s[1])).Equals($d834);
+    Test('s[2]').Assert(Word(s[2])).Equals($dd1e);
+  end;
+
+
+  procedure JsonStringDecoding.TabDecodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.DecodeString('\t');
+
+    Test('s').Assert(s).Equals(#9);
+  end;
+
+
+
+end.
---
+++ tests/Test.JsonStringEncoding.pas
@@ -0,0 +1,169 @@
+unit Test.JsonStringEncoding;
+
+interface
+
+  uses
+    Deltics.Smoketest;
+
+
+  type
+    JsonStringEncoding = class(TTest)
+      procedure AsciiCharacterStringsEncodeWithoutEscaping;
+      procedure DoubleQuoteEncodesWithEscaping;
+      procedure ReverseSolidusEncodesWithEscaping;
+      procedure SolidusEncodesWithEscaping;
+      procedure BackspaceEncodesWithEscaping;
+      procedure FormFeedEncodesWithEscaping;
+      procedure LineFeedEncodesWithEscaping;
+      procedure CarriageReturnEncodesWithEscaping;
+      procedure TabEncodesWithEscaping;
+      procedure OtherControlCharactersEncodeWithEscapedUnicode;
+      procedure NonAsciiCharactersEncodeWithEscapedUnicode;
+      procedure SupplementaryCharacterStringsEncodeWithEscapedUnicode;
+    end;
+
+
+
+implementation
+
+  uses
+    Deltics.Strings,
+    Deltics.Json;
+
+
+
+{ JsonStringEncoding }
+
+  procedure JsonStringEncoding.AsciiCharacterStringsEncodeWithoutEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString('abc def');
+
+    Test('s').Assert(s).Equals('"abc def"');
+  end;
+
+
+  procedure JsonStringEncoding.SolidusEncodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString('/');
+
+    Test('s').Assert(s).Equals('"\/"');
+  end;
+
+
+  procedure JsonStringEncoding.BackspaceEncodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString(#8);
+
+    Test('s').Assert(s).Equals('"\b"');
+  end;
+
+
+  procedure JsonStringEncoding.CarriageReturnEncodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString(#13);
+
+    Test('s').Assert(s).Equals('"\r"');
+  end;
+
+
+  procedure JsonStringEncoding.DoubleQuoteEncodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString('"');
+
+    Test('s').Assert(s).Equals('"\""');
+  end;
+
+
+  procedure JsonStringEncoding.FormFeedEncodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString(#12);
+
+    Test('s').Assert(s).Equals('"\f"');
+  end;
+
+
+  procedure JsonStringEncoding.LineFeedEncodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString(#10);
+
+    Test('s').Assert(s).Equals('"\n"');
+  end;
+
+
+  procedure JsonStringEncoding.NonAsciiCharactersEncodeWithEscapedUnicode;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString('©');
+
+    Test('s').Assert(s).Equals('"\u00A9"');
+  end;
+
+
+  procedure JsonStringEncoding.OtherControlCharactersEncodeWithEscapedUnicode;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString(#11);
+
+    Test('s').Assert(s).Equals('"\u000B"');
+  end;
+
+
+  procedure JsonStringEncoding.ReverseSolidusEncodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString('\');
+
+    Test('s').Assert(s).Equals('"\\"');
+  end;
+
+
+  procedure JsonStringEncoding.SupplementaryCharacterStringsEncodeWithEscapedUnicode;
+  {
+    For this test we use Unicode codepoint Ux1d11e - the treble clef
+
+      UTF-8 Encoding  :	0xF0 0x9D 0x84 0x9E
+      UTF-16 Encoding :	0xD834 0xDD1E
+  }
+  var
+    src: UnicodeString;
+    s: UnicodeString;
+  begin
+    src  := '??';
+    src[1] := WideChar($d834);
+    src[2] := WideChar($dd1e);
+
+    s := Json.EncodeString(src);
+
+    Test('s').Assert(s).Equals('"\uD834\uDD1E"');
+  end;
+
+
+  procedure JsonStringEncoding.TabEncodesWithEscaping;
+  var
+    s: UnicodeString;
+  begin
+    s := Json.EncodeString(#9);
+
+    Test('s').Assert(s).Equals('"\t"');
+  end;
+
+
+
+end.

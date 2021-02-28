4659d727cf0b0201331d82dc88097aff960f0f0f
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-02-28 20:27:23 +1300

    feat: JsonArray

--- src/Deltics.Json.Array_.pas
+++ src/Deltics.Json.Array_.pas
@@ -17,6 +17,7 @@ interface
     TJsonArray = class(TJsonCollection, IJsonArray)
     // IJsonArray
     protected
+      function get_AsStringArray: TUnicodeStringArray;
       function get_First: IJsonMutableValue;
       function get_Item(const aIndex: Integer): IJsonMutableValue;
       function get_Last: IJsonMutableValue;
@@ -24,6 +25,7 @@ interface
       function Add(const aValue: IJsonValue): Integer;
       procedure Delete(const aIndex: Integer);
       procedure Remove(const aValue: IJsonValue);
+      property Items[const aIndex: Integer]: IJsonMutableValue read get_Item;
     end;
 
 
@@ -66,6 +68,16 @@ implementation
         Delete(i);
         EXIT;
       end;
+  end;
+
+
+  function TJsonArray.get_AsStringArray: TUnicodeStringArray;
+  var
+    i: Integer;
+  begin
+    SetLength(result, Count);
+    for i := 0 to Pred(Count) do
+      result[i] := Items[i].AsString;
   end;
 
 
